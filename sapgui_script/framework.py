c2707e4d0370567c546bb76e35712f580529c30c
YuriyRum <rumyua@gmail.com>
2017-07-11 12:23:18 +0300

    log is written to a file

--- sapgui_script/framework.py
+++ sapgui_script/framework.py
@@ -1,5 +1,6 @@
 from win32com import client
 import csv
+from datetime import datetime
 
 class Runnable:
     def run(self, ses, code, row):
@@ -13,7 +14,8 @@ class Transaction:
         self.sapGui = client.GetObject("SAPGUI").GetScriptingEngine
         self.ses = self.sapGui.FindById("ses[0]")
         
-    def runScript(self, runnable):                
+    def runScript(self, runnable):
+        log = open("log_"+str(datetime.now()).replace(" ","_").replace(":","_")+".txt", "w")
         with open(self.path) as csvfile:                    
             reader = csv.DictReader(csvfile, delimiter = ';')
             for row in reader:
@@ -21,5 +23,7 @@ class Transaction:
                     runnable.run(self.ses, self.tcode, row)
                 except:
                     print(self.ses.FindById("wnd[0]/sbar").text)
+                    log.write(self.ses.FindById("wnd[0]/sbar").text+"\n")                    
+                    log.close()
         
     
